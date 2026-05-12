99f0db719e314d0e88594ea9806497ee27f22b38
Ashan Gunatilake <manuruddhaashan@gmail.com>
2026-05-12 23:39:14 +0530

    created user service endpoints

---
+++ services/user-service/src/main/java/com/project/user/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package com.project.user.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class UserResponse {
+
+    private UUID id;
+    private String email;
+    private String fullName;
+    private String phone;
+    private String role;
+    private String profilePictureUrl;
+}
