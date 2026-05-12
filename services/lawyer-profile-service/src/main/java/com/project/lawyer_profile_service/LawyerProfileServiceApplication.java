43a78e269e096f7871d70af98e83ee106f8e2ed2
UdithaPJ <udithapj@gmail.com>
2026-05-12 13:51:01 +0530

    lawyer-profile-service setup/1

---
+++ services/lawyer-profile-service/src/main/java/com/project/lawyer_profile_service/LawyerProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.lawyer_profile_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawyerProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawyerProfileServiceApplication.class, args);
+	}
+
+}
---
+++ services/lawyer-profile-service/src/test/java/com/project/lawyer_profile_service/LawyerProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lawyer_profile_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawyerProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
