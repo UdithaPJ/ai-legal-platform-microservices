1ba46172f18059491051e5c07c0b46e63f54f410
UdithaPJ <udithapj@gmail.com>
2026-05-19 13:19:42 +0530

    merged services

---
+++ services/video-session-service/src/test/java/com/project/videosessionservice/VideoSessionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.videosessionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSessionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
